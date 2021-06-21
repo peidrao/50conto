cc57a346ff93c1a6d894686cf3d544e954c7ae44
Your Name <you@example.com>
2021-06-21 19:34:55 -0300

    add model car

--- car/models.py
+++ car/models.py
@@ -2,9 +2,29 @@ from modelAbs import ModelAbs
 from django.db import models
 # Create your models here.
 
-class Car(ModelAbs):
-    title = models.CharField(max_length=50, null=False, blank=False)
 
+
+class Car(ModelAbs):
+    STATUS_CAR = (
+        (1, 'ativado'),
+        (2, 'desativado'),
+          
+    )
+
+    title = models.CharField(max_length=50, null=False, blank=False)
+    carName = models.CharField(unique=True, max_length = 20,null=True, blank=False)
+    plaque = models.CharField(unique=True, max_length = 10,null=True, blank=False)
+    car_model = models.CharField(unique=True, max_length = 20,null=True, blank=False)
+    color = models.CharField(unique=True, max_length = 20,null=True, blank=False)
+    vehicle_year = models.DateField(null=True)
+    mileage = models.CharField(unique=True, max_length = 20,null=True, blank=False)
+    status_car = models.IntegerField(choices=STATUS_CAR,null=False)
+    initial_date = models.DateField(null=True)
+    finish_date = models.DateField(null=True)
+
+    def __str__(self):
+        return f'MODELO: {self.car_model} - COR: {self.color}'
+    
 
 class ImageCar(ModelAbs):
     car_id = models.ForeignKey(Car, on_delete=models.CASCADE)
