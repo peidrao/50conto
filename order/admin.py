3ab5fa16cfb5d97837c8781744a779affc947e3c
joseandre <jose.andre.psn@gmail.com>
2021-06-23 11:17:52 -0300

    adding class OrderCar and ShopCar

--- order/admin.py
+++ order/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from order.models import Order
+from order.models import Order, ShopCar, OrderCar
 
 
 # Register your models here.
 admin.site.register(Order)
+admin.site.register(ShopCar)
+admin.site.register(OrderCar)
