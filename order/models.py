bc351775ff9de14f96ed48e41612ac18938532ce
Pedro <contatopedrorn@gmail.com>
2021-07-08 20:49:04 -0300

    order

--- order/models.py
+++ order/models.py
@@ -1,83 +1,101 @@
 from django.db import models
-from django.db.models.deletion import CASCADE
 from modelAbs import ModelAbs
 from user.models import User
 from car.models import Car
 
 # Create your models here.
-class Order(ModelAbs):
-  STATUS_ORDER = (
-      (1, 'Pagamento feito'),
-      (2, 'Aguardando pagamento'),
-      (3, 'Cancelar'),
-  )
-  STATE_ORDER = (
-      (1, 'AC - Acre'),
-      (2, 'AL - Alagoas'),
-      (3, 'AP - Amapá'),
-      (4, 'AM - Amazonas'),
-      (5, 'BA - Bahia'),
-      (6, 'CE - Ceará'),
-      (7, 'DF - Distrito Federal'),
-      (8, 'ES - Espírito Santo'),
-      (9, 'GO - Goiás'),
-      (10, 'MA - Maranhão'),
-      (11, 'MT - Mato Grosso'),
-      (12, 'MS - Mato Grosso do Sul'),
-      (13, 'MG - Minas Gerais'),
-      (14, 'PA - Pará'),
-      (15, 'PB - Paraíba'),
-      (16, 'PR - Paraná'),
-      (17, 'PE - Pernambuco'),
-      (18, 'PI - Piauí'),
-      (19, 'RJ - Rio de Janeiro'),
-      (20, 'RN - Rio Grande do Norte'),
-      (21, 'RS - Rio Grande do Sul'),
-      (22, 'RO - Rondônia'),
-      (23, 'RR - Roraima'),
-      (24, 'SC - Santa Catarina'),
-      (25, 'SP - São Paulo'),
-      (26, 'SE - Sergipe'),
-      (27, 'TO - Tocantins'),
-  )
-  
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
-#   car = models.ForeignKey(Car, on_delete=models.CASCADE)
-  total = models.FloatField(null=False)
-  status_order = models.IntegerField(choices=STATUS_ORDER, null=True, default=2)
-  first_name = models.CharField(max_length=50, null=False, blank=False)
-  last_name = models.CharField(max_length=50, null=False, blank=False)
-  city = models.CharField(max_length=75, null=False, blank=False)
-  state_order = models.IntegerField(choices=STATE_ORDER, null=False)
-  address = models.CharField('Endereço', max_length=250, null=False, blank=False)
-  number = models.CharField('Nº telefone celular', max_length=11, null=False, blank=False)
-  zip_code  = models.CharField('CEP', max_length=11, null=False, blank=False)
-  code = models.CharField('CEP', max_length=11, null=False, blank=False)
 
-  def __str__(self) -> str:
-      return self.zip_code
+
+class Order(ModelAbs):
+    STATUS_ORDER = (
+        (1, 'Pagamento em processamento'),
+        (2, 'Aguardando pagamento'),
+        (3, 'Pagamento feito'),
+        (4, 'Cancelar'),
+    )
+    STATE_ORDER = (
+        (1, 'AC - Acre'),
+        (2, 'AL - Alagoas'),
+        (3, 'AP - Amapá'),
+        (4, 'AM - Amazonas'),
+        (5, 'BA - Bahia'),
+        (6, 'CE - Ceará'),
+        (7, 'DF - Distrito Federal'),
+        (8, 'ES - Espírito Santo'),
+        (9, 'GO - Goiás'),
+        (10, 'MA - Maranhão'),
+        (11, 'MT - Mato Grosso'),
+        (12, 'MS - Mato Grosso do Sul'),
+        (13, 'MG - Minas Gerais'),
+        (14, 'PA - Pará'),
+        (15, 'PB - Paraíba'),
+        (16, 'PR - Paraná'),
+        (17, 'PE - Pernambuco'),
+        (18, 'PI - Piauí'),
+        (19, 'RJ - Rio de Janeiro'),
+        (20, 'RN - Rio Grande do Norte'),
+        (21, 'RS - Rio Grande do Sul'),
+        (22, 'RO - Rondônia'),
+        (23, 'RR - Roraima'),
+        (24, 'SC - Santa Catarina'),
+        (25, 'SP - São Paulo'),
+        (26, 'SE - Sergipe'),
+        (27, 'TO - Tocantins'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+
+    status_order = models.IntegerField(
+        choices=STATUS_ORDER, null=True, default=1)
+    
+    # personal information
+    first_name = models.CharField(max_length=50, null=False, blank=False)
+    last_name = models.CharField(max_length=50, null=False, blank=False)
+
+    # address 
+    city = models.CharField(max_length=75, null=False, blank=False)
+    state_order = models.IntegerField(choices=STATE_ORDER, null=False)
+    address = models.CharField(
+        'Endereço', max_length=250, null=False, blank=False)
+    number = models.CharField('Nº telefone celular',
+                              max_length=11, null=False, blank=False)
+    zip_code = models.CharField('CEP', max_length=11, null=False, blank=False)
+
+    code = models.CharField('Código', max_length=11, null=False, blank=False)
+    
+    total = models.FloatField(null=False)
+
+    # payment
+    number_cart = models.CharField(max_length=16, null=False)
+    name_cart = models.CharField(max_length=30, null=False)
+    code_cart = models.CharField(max_length=3, null=False)
+    expiration_cart = models.CharField(max_length=5, null=False)
+
+    def __str__(self) -> str:
+        return self.zip_code
 
 
 class ShopCart(ModelAbs):
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
-  car = models.ForeignKey(Car, on_delete=models.CASCADE)  
-  quantity = models.IntegerField(null=False)
-  
-  @property
-  def price(self):
-      return (self.car.price_day)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False)
 
-  def __str__(self) -> str:
-      return self.car.car_model
+    @property
+    def price(self):
+        return (self.car.price_day)
+
+    def __str__(self) -> str:
+        return self.car.car_model
 
 
 class OrderCar(ModelAbs):
 
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
-  car = models.ForeignKey(Car, on_delete=models.CASCADE)  
-  order = models.ForeignKey(Order, on_delete=models.CASCADE) 
-  quantity = models.IntegerField(null=False, blank=False)
-  price = models.FloatField(null=False, blank=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False, blank=False)
+    price = models.FloatField(null=False, blank=False)
 
-  def __str__(self) -> str:
-      return self.car.carName
+    def __str__(self) -> str:
+        return self.car.carName
--- order/views.py
+++ order/views.py
@@ -1,10 +1,13 @@
+from datetime import datetime
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib import messages
+from django.core import exceptions
 from django.db import connection
 from django.http.response import HttpResponse
 from django.utils.crypto import get_random_string
 from django.shortcuts import HttpResponseRedirect
 from django.shortcuts import render
+from django.core.exceptions import ValidationError
 from django.views import generic
 
 from order.models import Order, OrderCar, ShopCart
@@ -13,35 +16,46 @@ from order.forms import CreateOrderForm
 from user.models import User
 from car.models import Car
 
+
 class AddCartInShopCartView(SuccessMessageMixin, generic.View):
     def post(self, request, id):
-        checkproduct = ShopCart.objects.raw(f"SELECT * FROM order_shopcart WHERE car_id = {id}")       
+        user = User.objects.raw(f'SELECT * FROM user_user WHERE id = {request.user.id}')[0]
 
-        if checkproduct:
-            control = 1
+        if user.type_user == 1:
+            checkproduct = ShopCart.objects.raw(
+                f"SELECT * FROM order_shopcart WHERE car_id = {id}")
+
+            if checkproduct:
+                control = 1
+            else:
+                control = 0
+
+            if control == 1:
+                data = ShopCart.objects.raw(
+                    f"SELECT * FROM order_shopcart WHERE car_id = {id}")[0]
+                data.quantity += int(request.POST.get('quantity'))
+                data.save()
+                return HttpResponseRedirect('/cart')
+            else:
+                data = ShopCart()
+                data.user_id = request.user.id
+                data.car_id = id
+                data.quantity = int(request.POST.get('quantity'))
+                data.save()
+                messages.success(request, 'Carro adicionado a sua conta')
+                return HttpResponseRedirect('/cart')
         else: 
-            control = 0
+            messages.warning(request, 'Você é um locatário!')
+            return HttpResponseRedirect(f'/car_detail/{id}')
 
-        if control == 1:
-            data = ShopCart.objects.raw(f"SELECT * FROM order_shopcart WHERE car_id = {id}")[0]
-            data.quantity += int(request.POST.get('quantity'))
-            data.save()
-            return HttpResponseRedirect('/cart')
-        else:
-            data = ShopCart()
-            data.user_id = request.user.id
-            data.car_id = id
-            data.quantity = int(request.POST.get('quantity'))
-            data.save()
-            messages.success(request, 'Carro adicionado a sua conta')
-            return HttpResponseRedirect('/cart')
 
-class CartView(generic.View): 
+class CartView(generic.View):
     model = ShopCart
     template_name = 'cart.html'
 
     def get(self, request, *args, **kwargs):
-        shopcart = ShopCart.objects.raw(f'SELECT * FROM order_shopcart WHERE user_id = {request.user.id}')
+        shopcart = ShopCart.objects.raw(
+            f'SELECT * FROM order_shopcart WHERE user_id = {request.user.id}')
         total = 0
         for cart in shopcart:
             total = cart.car.price_day * cart.quantity
@@ -53,25 +67,24 @@ class CartView(generic.View):
 
         return render(request, self.template_name, context)
 
-    
+
 class DeleteCartView(generic.DeleteView):
     def post(self, request, *args, **kwargs):
         try:
             with connection.cursor() as cursor:
                 id = kwargs['pk']
-                cursor.execute('DELETE FROM order_shopcart WHERE id = %s', [id])
+                cursor.execute(
+                    'DELETE FROM order_shopcart WHERE id = %s', [id])
                 return HttpResponseRedirect('/cart')
         except Exception as error:
             return HttpResponse(error)
-        
 
 
 class CreateOrderView(generic.CreateView):
     template_name = "order_form.html"
-    form_class = CreateOrderForm
 
     def get(self, request, *args, **kwargs):
-        form = self.form_class(request.POST)
+        # form = self.form_class(request.POST)
         shopcart = ShopCart.objects.filter(user_id=request.user.id)
         user = User.objects.get(id=request.user.id)
         total = 0
@@ -81,64 +94,47 @@ class CreateOrderView(generic.CreateView):
         context = {
             'total': total,
             'user': user,
-            'form': form,
+            # 'form': form,
             'list': Order
         }
-
-        # import pdb;pdb.set_trace()
 
         return render(request, self.template_name, context)
 
     def post(self, request, *args, **kwargs):
-        form = self.form_class(request.POST)
-        shopcart = ShopCart.objects.filter(user_id=request.user.id)
-        # car = Car.objects.get(user_id=request.user.id)
-        
-        total = 0
-        for cart in shopcart:
-            total = cart.car.price_day * cart.quantity
-        if form.is_valid():
-            data = Order()
+        try:
+            shopcart = ShopCart.objects.get(user_id=request.user.id)
             
-            data.first_name = form.cleaned_data['first_name']
-            data.last_name = form.cleaned_data['last_name']
-            data.address = form.cleaned_data['address']
-            data.state_order = form.cleaned_data['state_order']
-            data.city = form.cleaned_data['city']
-            data.number = form.cleaned_data['number']
-            data.zip_code = form.cleaned_data['zip_code']
-            data.user_id = request.user.id
-            data.total = total
-            ordercode = get_random_string(5).upper()
-            data.code = ordercode
-            data.save()
-            for rs in shopcart:
-                detail = OrderCar()
-               
-                detail.order_id = data.id
-                detail.car_id = rs.car.id
-                detail.user_id = request.user.id
-                detail.quantity = rs.quantity
-                detail.price = rs.price
-                detail.save()
 
-                car = Car.objects.get(id=rs.car_id)
-                car.status_car = 2
-                car.save()
-            ShopCart.objects.filter(user_id=request.user.id).delete()
+            user_id = request.user.id
+            car_id = shopcart.car.id
+            status_order = 1
+            total_price = shopcart.car.price_day * shopcart.quantity
+            created_at = datetime.now()
+            updated_at = datetime.now()
+            code = get_random_string(5).upper()
+            state_order = request.POST['state_order']
+            first_name = request.POST['first_name']
+            last_name = request.POST['last_name']
+            city = request.POST['city']
+            address = request.POST['address']
+            number = request.POST['number']
+            zip_code = request.POST['zip_code']
 
-            # request.session['cart_items'] = 0
+            number_cart = request.POST['number_cart']
+            name_cart = request.POST['name_cart']
+            code_cart = request.POST['code_cart']
+            expiration_cart = request.POST['expiration_cart']
 
-            messages.success(
-                request, 'Your order has been completed, Thank You')
+            with connection.cursor() as cursor:
+                cursor.execute("INSERT INTO order_order VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s,%s,%s,%s,%s,%s,%s, %s, %s, %s, %s)",[
+                    None, created_at, updated_at, total_price, first_name, last_name, city, state_order, address, number, zip_code, code, 
+                    user_id, car_id, code_cart, expiration_cart, name_cart, number_cart, status_order
+                ])
 
-            context = {
-                'ordercode': ordercode,
-            }
+                cursor.execute(
+                    'DELETE FROM order_shopcart WHERE user_id = %s', [request.user.id])
 
-            return render(request, 'order_completed.html', context)
-        else:
-            messages.warning(request, form.errors)
-            return HttpResponseRedirect('/order/orderbook')
 
-            
+            return render(request, 'order_completed.html', { 'ordercode': code})
+        except Exception as error:
+            raise ValidationError(error)
